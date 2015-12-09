ef52ec43b04bebfbb19a8bde7ec9f99233a21b80
Nate Vagle <natevagle@gmail.com>
2015-12-08 20:47:16 -0600

    Adding Project to GitHub
    Initial Project Commit

---
+++ trunk/MyJourney.Web/assets/plugins/backstretch/backstretch-ini.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  $.getScript("assets/plugins/backstretch/jquery.backstretch.min.js", function() {
+    $(".fullscreen-static-image").backstretch("assets/images/background_Julie.jpg");
+  });
+});
